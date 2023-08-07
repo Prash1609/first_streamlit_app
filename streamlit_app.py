3760589681704a6b78c2d82534e209f4d9e8e591
Prash1609 <85450767+Prash1609@users.noreply.github.com>
2023-08-07 14:45:42 +0530

    Update streamlit_app.py
    Updated the right variable - correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,10 +23,10 @@ streamlit.dataframe(fruits_to_show)
 #Fruityvice advice display
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like information about?', Kiwi)
 streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 # Convert Json to flattened table
