c2e82d74fc57a17f5713216fe10dc634abb77e1b
Prash1609 <85450767+Prash1609@users.noreply.github.com>
2023-08-08 11:15:37 +0530

    Update streamlit_app.py
    Added try except to select which code to run when app reloads

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,16 +25,20 @@ streamlit.dataframe(fruits_to_show)
 #Fruityvice advice display
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)  
+    # Convert Json to flattened table
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # Print table to streamlit
+    streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json())
-
-# Convert Json to flattened table
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Print table to streamlit
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error(e)
+  
 
 #troubleshooting
 streamlit.stop()
